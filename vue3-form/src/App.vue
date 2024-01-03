60fb7cc09817d38adfda23a97e3069c292c265e0
tajon1030 <da33202@gmail.com>
2024-01-03 22:50:49 +0900

    섹션7. 프로젝트 생성 및 로그인 폼 UI 구성
    v-model

--- vue3-form/src/App.vue
+++ vue3-form/src/App.vue
@@ -1,68 +1,28 @@
 <template>
-  <form action="" @submit.prevent="submitForm">
+  <form action="">
     <div>
-      <label for="userId">ID:</label>
-      <input id="userId" type="text" v-model="username">
+      <label for="usrId">ID: </label>
+      <input name="usrId" type="text" v-model="usrname">
     </div>
     <div>
-      <label for="password">PW:</label>
-      <input type="text" v-model="password">
+      <label for="pwd">PWD: </label>
+      <input type="password" name="pwd" v-model="pwd">
     </div>
     <button type="submit">로그인</button>
   </form>
 </template>
 
 <script>
-import axios from 'axios'
-import { ref } from 'vue';
-
   export default {
-    setup() {
-      // data
-      var username = ref('');
-      var password = ref('');
-
-      // methods
-      var submitForm = () => {
-        axios.post('https://jsonplaceholder.typicode.com/users/', {
-          username: username.value,
-          password: password.value
-        }).then(response => {
-          console.log(response);
-        })
+    data() {
+      return {
+        usrname: '',
+        pwd: ''
       }
-      
-      return { username, password, submitForm }
     },
-    // methods: {
-    //   logText() {
-    //     this.pas
-    //   }
-    // }
-
-    // data() {
-    //   return {
-    //     username: '',
-    //     password: '',
-    //   }
-    // },
-    // methods: {
-    //   submitForm() {
-    //     // event.preventDefault();
-    //     const data = {
-    //       username: this.username,
-    //       password: this.password,
-    //     }
-    //     axios.post('https://jsonplaceholder.typicode.com/users/', data)
-    //       .then(response => {
-    //         console.log(response)
-    //       });
-    //     // console.log('제출됨')
-    //   }
-    // }
   }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
 
 </style>
