02aab7b116297b105cf63b6b9834f494e9f4694d
tajon1030 <da33202@gmail.com>
2024-01-03 22:14:22 +0900

    섹션 6. 싱글파일컴포넌트 코드작성 팁
    v-on:click 줄임말 @click
    vue 스니펫 vbc / vda

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,32 +1,25 @@
-<!-- HTML -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ msg }}
+  </div>
+  <button @click="showAlert">경고</button>
 </template>
 
-<!-- JavaScript-->
 <script>
-// import 라이브러리 from '라이브러리이름'
-// import 파일이름 from './파일경로'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  // 인스턴스 옵션 속성
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        msg: 'hi'
+      }
+    },
+    methods :{
+      showAlert(){
+        alert('hello');
+      }
+    }
   }
-}
 </script>
 
-<!-- CSS -->
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
