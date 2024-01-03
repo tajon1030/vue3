0e820989463bec35987c6f183dd048326f019180
tajon1030 <da33202@gmail.com>
2024-01-03 22:04:30 +0900

    섹션6. 싱글파일 컴포넌트

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,12 +1,17 @@
+<!-- HTML -->
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
+<!-- JavaScript-->
 <script>
+// import 라이브러리 from '라이브러리이름'
+// import 파일이름 from './파일경로'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  // 인스턴스 옵션 속성
   name: 'App',
   components: {
     HelloWorld
@@ -14,6 +19,7 @@ export default {
 }
 </script>
 
+<!-- CSS -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
