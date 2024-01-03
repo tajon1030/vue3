110aed38aa8457ae1efe36090193719720db8730
tajon1030 <da33202@gmail.com>
2024-01-03 23:08:20 +0900

    섹션7. 폼이벤트 제어 및 서버로 데이터 전송
    axios, @submit

--- vue3-form/src/App.vue
+++ vue3-form/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <form action="">
+  <form action="" @submit.prevent="submitForm">
     <div>
       <label for="usrId">ID: </label>
       <input name="usrId" type="text" v-model="usrname">
@@ -13,6 +13,9 @@
 </template>
 
 <script>
+// import 라이브러리명 from '패키지이름'
+  import axios from 'axios'
+
   export default {
     data() {
       return {
@@ -20,6 +23,21 @@
         pwd: ''
       }
     },
+    methods : {
+      submitForm(){
+//        event.preventDefault(); // $submit.prevent로 대체 (form submit의 기본동작 reload)
+
+        const data = {
+          username: this.usrname,
+          password: this.pwd,
+        }
+        axios.post('https://jsonplaceholder.typicode.com/users', data) // sampleAPI:jsonplaceholder
+          .then(response => {
+            console.log(response);
+          })
+//        console.log('제출됨');
+      }
+    }
   }
 </script>
 
