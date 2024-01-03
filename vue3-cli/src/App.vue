e7924f06165755a6c311151cfb5d19dd44041e5c
tajon1030 <da33202@gmail.com>
2024-01-03 22:23:48 +0900

    섹션 6. 컴포넌트 등록방법 및 명명 규칙

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <AppHeader></AppHeader>
   <div>
     {{ msg }}
   </div>
@@ -6,7 +7,17 @@
 </template>
 
 <script>
+// import 컴포넌트이름 from './컴포넌트 경로'
+  import AppHeader from './components/AppHeader.vue'
+
   export default {
+    components : {
+      // '컴포넌트이름' : 컴포넌트 내용
+      // 'app-header' : AppHeader // 네이밍컨벤션은 파스칼케이스
+      // 'AppHeader' : AppHeader  // 컴포넌트이름은 어짜피 문자이기때문에 따옴표 생략
+      // AppHeader : AppHeader // 앞뒤 같은 단어면 하나로 축약가능
+      AppHeader
+    },
     data() {
       return {
         msg: 'hi'
